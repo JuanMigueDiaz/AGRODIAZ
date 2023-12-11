6df8a4e804861c874b4e8bce3ad4bcf6a8397cb0
Juan Miguel <diazgutierrezjuanmiguel@gmail.com>
2023-12-11 06:50:22 -0500

    todo marcha bien, vamos incluir la plantilla fornt de AGRODIAZ

--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -3,6 +3,64 @@
 @endsection
 @section('content')
 <div class="content-wrapper">
+  
+
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Admin List</h1>
+          </div>
+          <div class="col-sm-6" style="text-align: right;">
+            <a hreft="{{ url('admin/admin/add')}}" class="btn btn-primary">Add new Admin</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+
+   
+          <div class="col-md-12">
+    
+            <!-- /.card -->
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Admin List</h3>
+              </div>
+       
+              <div class="card-body p-0">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Update software</td>
+                      <td>Update software</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AdminController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,17 +23,17 @@ Route::post('admin', [AuthController::class, 'auth_login_admin']);
 Route::get('admin/logout', [AuthController::class, 'logout_admin']); 
 
 
+
+
 Route::group(['middleware' => 'admin'], function() {
 
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']); 
    
+    Route::get('admin/admin/list', [AdminController::class, 'list']); 
+    Route::get('admin/admin/add', [AdminController::class, 'add']);
+    Route::post('admin/admin/add', [AdminController::class, 'insert']); 
 
-    
-    Route::get('admin/admin/list', function () {
-        $data['header_title'] = 'Admin';
-        return view('admin.admin.list', $data);
-    });
-    
+
 });
 
 
